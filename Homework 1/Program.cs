722cf152d1cb2d45b950a7ad637acf29e4634ba7
Fidan <fidan@MacBook-Pro-Fidan.local>
2022-12-11 00:21:00 +0400

    first commit

---
+++ Homework 1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Homework_1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number1,number2;
+        number1 = Convert.ToInt32(Console.ReadLine());
+        number2 = Convert.ToInt32(Console.ReadLine());
+        int sum = number1 + number2;
+        Console.WriteLine($"{number1}+{number2}={sum}");
+
+        Console.ReadKey();
+
+    }
+}
+
